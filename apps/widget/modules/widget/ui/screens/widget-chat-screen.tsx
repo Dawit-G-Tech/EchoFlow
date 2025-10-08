f412bebdcd08b2c532360e566595a0620aa4ed91
Dawit-G-Tech <dawit9514@gmail.com>
2025-10-08 22:08:47 +0300

    InfiniteScroll-hook and Trigger

--- apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
@@ -7,6 +7,9 @@ import {useThreadMessages, toUIMessages} from "@convex-dev/agent/react";
 import { Button } from "@workspace/ui/components/button";
 import { WidgetHeader } from "../components/widget-header";
 import { ArrowLeftIcon, MenuIcon } from "lucide-react";
+import { DicebearAvatar } from "@workspace/ui/components/dicebear-avatar";
+import { useInfiniteScroll } from "@workspace/ui/hooks/use-infinite-scroll";
+import { InfiniteScrollTrigger } from "@workspace/ui/components/infinite-scroll-trigger";
 import { useAtomValue, useSetAtom } from "jotai";
 import { contactSessionIdAtomFamily, conversationIdAtom, organizationIdAtom, screenAtom } from "../../atoms/widget-atoms";
 import { useAction, useQuery } from "convex/react";
@@ -70,6 +73,12 @@ export const WidgetChatScreen = () => {
             {initialNumItems : 10},
     );
 
+    const {topElementRef, handleLoadMore, canLoadMore, isLoadingMore} = useInfiniteScroll({
+        status: messages.status,
+        loadMore: messages.loadMore,
+        loadSize: 10,
+    });
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -112,6 +121,13 @@ export const WidgetChatScreen = () => {
             </WidgetHeader>
            <AIConversation>
                 <AIConversationContent>
+                    <InfiniteScrollTrigger
+                        canLoadMore={canLoadMore}
+                        isLoadingMore= {isLoadingMore}
+                        onLoadMore={handleLoadMore}
+                        ref={topElementRef}
+                    
+                    />
                     {toUIMessages(messages.results?? [])?.map(message => {
                         return (
                             <AIMessage
@@ -121,6 +137,14 @@ export const WidgetChatScreen = () => {
                                 <AIMessageContent>
                                     <AIResponse>{message.content}</AIResponse>
                                 </AIMessageContent>
+                                {message.role === "assistant" && (
+                                    <DicebearAvatar
+                                        imageUrl="/logo.svg"
+                                        seed="assistant"
+                                        size={32}
+                                    
+                                    />
+                                )}
                             </AIMessage>
                         )
                     })}
